bcf6b76e9ebc46e3fe9a814751ac5d55368a23df
juvenalmaiordosul-boop <juvenalmaiordosul@gmail.com>
2026-04-06 15:03:20 -0300

    Create deploy-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const { REST, Routes, SlashCommandBuilder } = require('discord.js');
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName('gamerover')
+    .setDescription('🚨 Evacuação total do servidor')
+].map(c => c.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log('Deploying slash command...');
+    await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID),
+      { body: commands }
+    );
+    console.log('Slash command deployed.');
+  } catch (err) {
+    console.error(err);
+  }
+})();
